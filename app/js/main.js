6e3e047a8a23c2bbb032cf6e5f71e86e5289c9b2
Evan Yan <1370204201@qq.com>
2016-03-01 18:33:33 +0800

    基本架子搭建完毕

--- app/js/main.js
+++ app/js/main.js
@@ -2,51 +2,74 @@
  * Created by yantianyu on 2016/3/1.
  */
 
-// define our application and pull in ngRoute and ngAnimate
-var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
-
-// ROUTING ===============================================
-// set our routing for this application
-// each route will pull in a different controller
-animateApp.config(function ($routeProvider) {
-
+var app = angular.module('app', ['ngRoute', 'ngAnimate']);
+app.config(function ($routeProvider) {
     $routeProvider
-
-        // home page
         .when('/home', {
             templateUrl: './views/page-home.html',
             controller: 'mainController'
         })
-
-        // about page
         .when('/about', {
             templateUrl: './views/page-about.html',
             controller: 'aboutController'
         })
-
-        // contact page
         .when('/contact', {
             templateUrl: './views/page-contact.html',
             controller: 'contactController'
         })
+        .when('/question/:questionId',{
+            templateUrl:'./views/question.html',
+            controller:'questionController'
+        })
 
-        .otherwise('/home');
+        //.otherwise('/home');
 
 });
-
-
-// CONTROLLERS ============================================
-// home page controller
-animateApp.controller('mainController', function ($scope) {
+app.service('questionService',function(){
+    this.data = [];
+});
+app.controller('mainController', function ($scope) {
     $scope.pageClass = 'page-home';
 });
-
-// about page controller
-animateApp.controller('aboutController', function ($scope) {
+app.controller('aboutController', function ($scope) {
     $scope.pageClass = 'page-about';
 });
-
-// contact page controller
-animateApp.controller('contactController', function ($scope) {
+app.controller('contactController', function ($scope) {
     $scope.pageClass = 'page-contact';
+});
+app.controller('questionController',function($scope, $http, $routeParams, $rootScope, questionService){
+    $rootScope.activeIndex = $routeParams.questionId;
+    $scope.question = questionService.data[$routeParams.questionId-1];
+    $scope.toggle = function(){};
+});
+
+app.controller('navbarController',function($scope, questionService, $location,$rootScope){
+    $scope.hover = '';
+    $scope.questions = questionService;
+    $scope.toggleSerialCard = function(){
+        if($scope.hover == ''){
+            $scope.hover = 'hover';
+        }
+        else{
+            $scope.hover = '';
+        }
+    };
+    $scope.clearHover = function(){
+        $scope.hover = '';
+    };
+    $scope.preQue = function(){
+        $location.path('question/'+($rootScope.activeIndex-1));
+    };
+    $scope.nextQue = function(){
+        $location.path('question/'+(parseInt($rootScope.activeIndex)+1));
+    };
+});
+
+app.run(function($http,questionService){
+    $http({
+        method:'GET',
+        url:'/data/test_data.json'
+    }).then(function(resp){
+        questionService.data = resp.data;
+    });
 });
